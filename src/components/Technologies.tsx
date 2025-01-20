b8d02a53277939014e5345404b506c0621362450
Harshil Sondagar <sondagarharshil0@gmail.com>
2025-01-21 01:00:52 +0530

    @

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -79,7 +79,7 @@ const Technologies = () => {
   };
 
   return (
-    <section className="py-20 px-4 bg-background relative">
+    <section className="py-20 px-4 relative">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
